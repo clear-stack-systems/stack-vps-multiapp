57de6211ebc78371a7e48a3071b1e57dfea73d4f
bence <dev.ben.h@gmail.com>
2026-01-04 16:55:35 +0100

    Make certbot use the env file

--- scripts/first-time-certbot.sh
+++ scripts/first-time-certbot.sh
@@ -19,7 +19,13 @@ email="${CERTBOT_EMAIL:-admin@${DOMAIN_PROD}}"
 request_cert() {
   local domain="$1"
   echo "[certbot] Requesting cert for ${domain}..."
-  docker compose run --rm certbot certonly --webroot     -w /var/www/certbot     -d "${domain}"     --email "${email}"     --agree-tos     --no-eff-email
+  docker compose --env-file "${ENV_FILE}" -f docker-compose.yml -f docker-compose.server.yml \
+    run --rm certbot certonly --webroot \
+    -w /var/www/certbot \
+    -d "${domain}" \
+    --email "${email}" \
+    --agree-tos \
+    --no-eff-email
 }
 
 request_cert "${DOMAIN_PROD}"
@@ -30,6 +36,7 @@ if [[ -n "${DOMAIN_N8N:-}" ]]; then
 fi
 
 echo "[certbot] Reloading nginx..."
-docker compose exec nginx nginx -s reload
+docker compose --env-file "${ENV_FILE}" -f docker-compose.yml -f docker-compose.server.yml \
+  exec nginx nginx -s reload
 
 echo "[certbot] Done."
